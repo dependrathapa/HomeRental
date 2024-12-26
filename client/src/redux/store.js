2691f8d6fa4698f56cdf8eec44b57a3d5e61b4d8
dependtathapa <np03cs4a220246@heraldcollege.edu.np>
2024-12-26 01:06:13 -0500

    login and register connected to database.

---
+++ client/src/redux/store.js
@@ -0,0 +1,37 @@
+/* Whenever the user closes the tab or browser their information is still there and there whenever they reopne
+the tab and reopen the website then they can access their user state review and then they don't lost any information
+what they do in beginning and the only way to get rid of this is clear the catch */
+
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import state from "./state";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, state);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);

a68f085e8935ed2497fec0eb6927452f17570cb5
dependtathapa <np03cs4a220246@heraldcollege.edu.np>
2025-01-11 04:29:52 -0500

    Navbar and logout

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/Slide.scss"
+const Slide = () => {
+  return (
+    <div className='slide'>
+      <h1>घरमा स्वागत छ!<br/> तपाईं जहाँ पनि घुम्नुहुन्छ पल मा रहनुहोस् र आफ्नो सम्झनाहरू बनाउनुहोस्।</h1>
+    </div>
+  )
+}
+
+export default Slide
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar';
+import Slide from '../components/Slide';
 const HomePage = () => {
   return (
-    <div>
-      HomePage Dependra
-    </div>
+   <>
+   <Navbar />
+   <Slide />
+   </>
   )
 }
 
